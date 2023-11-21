c502ed77f9e404c9836f7bafdea856813c077b30
Simamnkele <smambo85@gmail.com>
2023-11-21 18:30:49 +0200

    DBstorage update on place, user, city classes

--- models/user.py
+++ models/user.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models
+import sqlalchemy
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    if models.base_model.storage_t == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialises user class."""
+        super().__init__(*args, **kwargs)
